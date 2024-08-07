6d266751f01f1b7f7c8c4044978334f615528f16
1116mp <167277996+1116mp@users.noreply.github.com>
2024-08-07 11:47:57 +0530

    Create FIBO.java

---
+++ FIBO.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    public static void main(String[] args) {
+        int number = 10; 
+        System.out.println("Fibonacci number at position " + number + " is: " + fibonacci(number));
+    }
+}
