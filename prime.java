01a543d8fab88289fccb188917e2400871b805bb
kshitijnangarevesit <2023.kshitij.nangare@ves.ac.in>
2024-10-08 23:54:23 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,36 @@
+// Java program to demonstrate Optimized method based to check if a number is prime
+import java.util.Scanner;
+
+class GFG {
+    static boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+         // For n=2 or n=3 it will check
+        if (n == 2 || n == 3)
+            return true;
+        // For multiple of 2 or 3 This will check 
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        // It will check all the others condition
+        for (int i = 5; i <= Math.sqrt(n); i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+
+        return true;
+    }
+
+    // Driver Program
+    public static void main(String args[])
+    {
+        if (isPrime(11))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+        if (isPrime(15))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+    }
+}
